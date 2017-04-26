01bb20063bf1527781d5d84a47bbd553d6adcbc9
Centimitr <centimitr@gmail.com>
2017-04-26 10:53:03 +0800

    no message

---
+++ src/_deprecated/mark/mark.component.ts
@@ -0,0 +1,26 @@
+import {Component, ElementRef, EventEmitter, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'mark',
+  templateUrl: './mark.component.html',
+  styleUrls: ['./mark.component.css']
+})
+export class MarkComponent implements OnInit {
+  elm: any;
+  inView: boolean = false;
+  @Output() enter = new EventEmitter<null>();
+  @Output() leave = new EventEmitter<null>();
+
+  constructor(elm: ElementRef) {
+    this.elm = elm.nativeElement;
+  }
+
+  ngOnInit() {
+    const io = new IntersectionObserver(() => {
+      this.inView = !this.inView;
+      if (this.inView) this.enter.emit();
+      else this.leave.emit();
+    });
+    io.observe(this.elm);
+  }
+}
